e3cc7266369ba90f7f99394c7847d5bb02dd9b39
CosmicScale <cosmicsocial@icloud.com>
2025-04-22 15:45:00 +0100

    Added wget and chromium for Arch and Fedora distros

--- 01-Setup.sh
+++ 01-Setup.sh
@@ -96,10 +96,10 @@ if [ -x "$(command -v apt)" ]; then
     sudo apt update && sudo apt install -y axel imagemagick xxd python3 python3-venv python3-pip nodejs npm bc rsync curl zip wget chromium
 # Or if user is on Fedora-based system, do this instead
 elif [ -x "$(command -v dnf)" ]; then
-    sudo dnf install -y gcc axel ImageMagick xxd python3 python3-devel python3-pip nodejs npm bc rsync curl zip
+    sudo dnf install -y gcc axel ImageMagick xxd python3 python3-devel python3-pip nodejs npm bc rsync curl zip wget chromium
 # Or if user is on Arch-based system, do this instead
 elif [ -x "$(command -v pacman)" ]; then
-    sudo pacman -Sy --needed archlinux-keyring && sudo pacman -S --needed axel imagemagick xxd python pyenv python-pip nodejs npm bc rsync curl zip
+    sudo pacman -Sy --needed archlinux-keyring && sudo pacman -S --needed axel imagemagick xxd python pyenv python-pip nodejs npm bc rsync curl zip wget chromium
 fi
 if [ $? -ne 0 ]; then
     echo
